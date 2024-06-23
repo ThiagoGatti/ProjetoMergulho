f76a36fe36538405b899b6926e3754b9d58f584b
Thiago <thiago.bafi@outlook.com>
2024-06-23 14:16:21 -0300

    add validation

--- controle/src/main/java/mergulho/controle/controllers/CilindroController.java
+++ controle/src/main/java/mergulho/controle/controllers/CilindroController.java
@@ -1,5 +1,8 @@
 package mergulho.controle.controllers;
 
+import jakarta.validation.Valid;
+import mergulho.controle.dtos.CilindroRecordDto;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import mergulho.controle.models.CilindroModel;
@@ -7,6 +10,7 @@ import mergulho.controle.repositories.CilindroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -31,44 +35,20 @@ public class CilindroController {
         return "cilindros/form";
     }
 
-    @GetMapping("/getCilindroID/{id}")
-    public ResponseEntity<CilindroModel> getCilindroID(@PathVariable Long id) {
-            Optional<CilindroModel> cilindroData = cilindroRepository.findById(id);
-
-            if (cilindroData.isPresent()) {
-                return new ResponseEntity<>(cilindroData.get(), HttpStatus.OK);
-            }
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-    @PostMapping("/addCilindro")
-    public ResponseEntity<CilindroModel> addCilindro(@RequestBody CilindroModel cilindro) {
-        CilindroModel cilindroObj = cilindroRepository.save(cilindro);
-        return new ResponseEntity<>(cilindroObj, HttpStatus.OK);
-
-    }
-    @PostMapping("/updateCilindroID/{id}")
-    public ResponseEntity<CilindroModel> updateCilindroID(@PathVariable Long id, @RequestBody CilindroModel newCilindroData) {
-
-        Optional<CilindroModel> cilindroData = cilindroRepository.findById(id);
-
-        if(cilindroData.isPresent()) {
-            CilindroModel updatedCilindroData = cilindroData.get();
-            updatedCilindroData.setDataFabricacao(newCilindroData.getDataFabricacao());
-            updatedCilindroData.setTipoCilindro(newCilindroData.getTipoCilindro());
-            updatedCilindroData.setSerialNum(newCilindroData.getSerialNum());
-
-            CilindroModel cilindroObj = cilindroRepository.save(updatedCilindroData);
-            return new ResponseEntity<>(cilindroObj, HttpStatus.OK);
+    @PostMapping("/cilindros")
+    public String saveCilindro(@Valid CilindroRecordDto cilindroRecordDto, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "cilindros/form";
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        CilindroModel cilindroModel = new CilindroModel();
+        BeanUtils.copyProperties(cilindroRecordDto, cilindroModel);
+        cilindroRepository.save(cilindroModel);
+        return "redirect:/cilindros";
     }
     @DeleteMapping("/deleteCilindroID/{id}")
     public ResponseEntity<HttpStatus> deleteCilindroID(@PathVariable Long id) {
         cilindroRepository.deleteById(id);
 
         return new ResponseEntity<>(HttpStatus.OK);
-
     }
-
-
 }
