937cdeb89806b3b64301f60084939f25936c0313
Your Name <thiago.bafi@outlook.com>
2025-03-05 20:27:21 -0300

    Update mask.js

--- controle/src/main/resources/static/js/mask.js
+++ controle/src/main/resources/static/js/mask.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function applyMasks() {
         $('#numeroCelular').mask('(00) 00000-0000');
-        $('#telefone').mask('(00) 0000-0000');
+        $('#telefone').mask('0000-0000');
         $('#cpf').mask('000.000.000-00');
         $('#rg').mask('00.000.000-0');
         $('#cep').mask('00000-000');
