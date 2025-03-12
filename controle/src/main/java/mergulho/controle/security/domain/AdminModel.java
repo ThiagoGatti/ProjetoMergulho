5cb62a1c7c02c080fad24390cc5a1c0584a11d74
Your Name <thiago.bafi@outlook.com>
2025-03-12 15:24:53 -0300

    Update AdminModel.java

--- controle/src/main/java/mergulho/controle/security/domain/AdminModel.java
+++ controle/src/main/java/mergulho/controle/security/domain/AdminModel.java
@@ -18,4 +18,36 @@ public class AdminModel {
     @Column(unique = true)
     private String email;
     private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
